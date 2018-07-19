e9fcde0997256e1b63e8009fc6a379639d0b59bd
Alex Elgin <sascha.elgin@gmail.com>
2018-07-19 22:16:13 +0600

    apply atomic design to the common component Img

--- app/common/atoms/Img/component/index.test.js
+++ app/common/atoms/Img/component/index.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import Img from '../index';
+import Img from './';
 
 Enzyme.configure({ adapter: new Adapter() });
 
--- app/common/organisms/Header/Img/component/index.js
+++ app/common/organisms/Header/Img/component/index.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import NormalImg from '../../../../../components/Img';
+import NormalImg from '../../../../atoms/Img';
 
 const Img = styled(NormalImg)`
   width: 100%;

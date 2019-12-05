c0087b736159a7f8a1a24eea3f8ea8f478c27750
Alex Elgin <sascha.elgin@gmail.com>
2019-12-05 17:01:44 +0600

    fix mobile layout

--- app/organisms/Header/component/index.jsx
+++ app/organisms/Header/component/index.jsx
@@ -1,27 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage, intlShape } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 
-import Img from '../Img';
 import NavBar from '../NavBar';
 import HeaderLink from '../HeaderLink';
 
-import Banner from './banner.jpg';
-import reactLogo from './react.svg';
-import reduxLogo from './redux.svg';
 import messages from './messages';
-import styles from './styles.scss';
 import LinksBlock from '../LinksBlock';
 
-const Header = ({ authenticated, intl }) => (
+const Header = ({ authenticated }) => (
   <div>
     <LinksBlock authenticated={authenticated} />
-    <Img src={Banner} alt={intl.formatMessage(messages.logo)} />
-    <div className={styles['logo-block']}>
-      <Img src={reactLogo} alt={intl.formatMessage(messages.logo)} className={styles['logo-react']} />
-      <Img src={reduxLogo} alt={intl.formatMessage(messages.logo)} className={styles['logo-redux']} />
-      <div className={styles['logo-text']}>React Redux<br />PWA Boilerplate</div>
-    </div>
     <NavBar>
       <HeaderLink to="/">
         <FormattedMessage {...messages.home} />
@@ -35,7 +24,6 @@ const Header = ({ authenticated, intl }) => (
 
 Header.propTypes = {
   authenticated: PropTypes.bool,
-  intl: intlShape.isRequired,
 };
 
 export default Header;

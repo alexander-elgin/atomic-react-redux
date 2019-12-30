fce789474fea6bc86e153619e983571d1dd71cdb
Alex Elgin <sascha.elgin@gmail.com>
2019-12-30 18:42:08 +0600

    validate the 'Sign Up' form

--- app/organisms/SignUpForm/index.jsx
+++ app/organisms/SignUpForm/index.jsx
@@ -18,6 +18,7 @@ import {
   Person,
 } from '@material-ui/icons';
 import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
+import * as yup from 'yup';
 
 import submitForm from '../../utils/form';
 import { post } from '../../utils/request';
@@ -27,10 +28,16 @@ import messages from './messages';
 const SignUpForm = ({ intl }) => {
   const { push } = useHistory();
 
+  const validationSchema = yup.object().shape({
+    email: yup.string().required().email().label(intl.formatMessage(messages.email)),
+    name: yup.string().required().label(intl.formatMessage(messages.name)),
+    password: yup.string().required().min(8).label(intl.formatMessage(messages.password)),
+  });
+
   return (
     <Formik
       initialValues={{ _error: '', email: '', name: '', password: '' }}
-      validate={() => ({})}
+      validationSchema={validationSchema}
       onSubmit={(values, formActions) => submitForm(
         () => post('/auth/signup', values),
         () => push('/signin'),
@@ -51,6 +58,7 @@ const SignUpForm = ({ intl }) => {
                     disabled={isSubmitting}
                     label={intl.formatMessage(messages.name)}
                     name="name"
+                    required
                     type="text"
                     InputProps={{
                       startAdornment: <InputAdornment position="start"><Person color="primary" /></InputAdornment>,
@@ -63,6 +71,7 @@ const SignUpForm = ({ intl }) => {
                     disabled={isSubmitting}
                     label={intl.formatMessage(messages.email)}
                     name="email"
+                    required
                     type="email"
                     InputProps={{
                       startAdornment: <InputAdornment position="start"><Email color="primary" /></InputAdornment>,
@@ -75,6 +84,7 @@ const SignUpForm = ({ intl }) => {
                     disabled={isSubmitting}
                     label={intl.formatMessage(messages.password)}
                     name="password"
+                    required
                     type="password"
                     InputProps={{
                       startAdornment: <InputAdornment position="start"><Lock color="primary" /></InputAdornment>,
